f8e5538f0685f0f1959c43e7071ef39460bac8c2
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-11-26 21:28:24 -0800

    Dockerfile

--- src/cmd/main/main.go
+++ src/cmd/main/main.go
@@ -26,6 +26,7 @@ func main() {
 	})
 
 	r := mux.NewRouter().StrictSlash(true)
+
 	r.HandleFunc("/verify", routes.Verify).Methods("POST")
 	r.HandleFunc("/status", routes.Status).Methods("POST")
 	r.HandleFunc("/pub", routes.GetPub).Methods("GET")
