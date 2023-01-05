9fbb10e6a84897a3b1dcb56ee75a22578834c890
Thomas Parker <91673095+soloth@users.noreply.github.com>
2023-01-04 19:24:53 -0800

    ClientSettings

--- internal/global/structs.go
+++ internal/global/structs.go
@@ -61,3 +61,27 @@ type SubAccountRequest struct {
 	ParentSignature     SignatureStruct `json:"parent_signature" bson:"parent_signature"`
 	SubAccountSignature SignatureStruct `json:"sub_account_signature" bson:"sub_account_signature"`
 }
+
+
+type ClientSettings struct {
+	AdminName                   string   `json:"admin_name" bson:"admin_name"`
+	ProjectName                 string   `json:"project_name" bson:"project_name"`
+	AdminTelegram               string   `json:"admin_telegram" bson:"admin_telegram"`
+	TeamSize                    int      `json:"team_size" bson:"team_size"`
+	ProjectCommunicationChannel string   `json:"project_communication_channel" bson:"project_communication_channel"`
+	ProjectWebsite              string   `json:"project_website" bson:"project_website"`
+	DappName                    string   `json:"dapp_name" bson:"dapp_name"`
+	RPCURL                      string   `json:"rpc_url" bson:"rpc_url"`
+	GasTokenSymbol              string   `json:"gas_token_symbol" bson:"gas_token_symbol"`
+	ChainID                     int      `json:"chain_id" bson:"chain_id"`
+	VM                          string   `json:"vm" bson:"vm"`
+	Package                     string   `json:"package" bson:"package"`
+	PackageOptions              []string `json:"package_options" bson:"package_options"`
+	MaxUsers                    int      `json:"max_users" bson:"max_users"`
+	PuffinClientAddress         string   `json:"puffin_client_address" bson:"puffin_client_address"`
+	PuffinCoreAddress           string   `json:"puffin_core_address" bson:"puffin_core_address"`
+	AdminWalletAddress          string   `json:"admin_wallet_address" bson:"admin_wallet_address"`
+	UUID                        int      `json:"UUID" bson:"UUID"`
+	Status                      string   `json:"status" bson:"status"`
+	PaymentExpiration           int      `json:"payment_expiration" bson:"payment_expiration"`
+}
